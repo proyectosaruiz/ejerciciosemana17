01a3c8c937dbbd8cd665e4da6a92df5ae1003e39
Amilcar J. Ruiz <98772743+AmilcarJRuiz@users.noreply.github.com>
2022-05-11 21:00:56 +0200

    generando entorno

--- Ejercicio1/ejercicio1/src/App.js
+++ Ejercicio1/ejercicio1/src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  useState([
+    [
+      {
+        id: 1,
+        task: "Ir a la compra",
+        done: false,
+      },
+      {
+        id: 2,
+        task: "Hacer ejercicios de react",
+        done: true,
+      },
+    ],
+  ]);
+
   return <div className="App"></div>;
 }
 
